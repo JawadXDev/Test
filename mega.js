df7506698f50e92dd9f7be0a886648613e62ff6e
JawadXDev <JawadXDev@outlook.com>
2025-04-10 07:58:20 +0500

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,16 +1,28 @@
 import * as mega from 'megajs';
 
-const auth = {
-    email: 'jawadbisma@outlook.com',
-    password: 'bismaxjawad300+',
-    userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
-};
+// Array of Mega credentials
+const megaCredentials = [
+    { email: 'gqm9i7i@tmpnator.live', password: 'subzero123' },
+    { email: 'yidrepeydi@gufum.com', password: 'subzero123' },
+    { email: 'vastebaspi@gufum.com', password: 'subzero123' },
+    { email: 'mistemalmo@gufum.com', password: 'subzero123' },
+    { email: 'hurderarte@gufum.com', password: 'subzero123' }
+];
 
 const upload = (data, name) => {
     return new Promise((resolve, reject) => {
         try {
+            // Randomly select credentials from the array
+            const randomCreds = megaCredentials[Math.floor(Math.random() * megaCredentials.length)];
+            
+            const auth = {
+                email: randomCreds.email,
+                password: randomCreds.password,
+                userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
+            };
+
             const storage = new mega.Storage(auth, () => {
-                data.pipe(storage.upload({name: name, allowUploadBuffering: true}));
+                data.pipe(storage.upload({ name: name, allowUploadBuffering: true }));
                 storage.on("add", (file) => {
                     file.link((err, url) => {
                         if (err) {
@@ -30,4 +42,3 @@ const upload = (data, name) => {
 };
 
 export { upload };
-
