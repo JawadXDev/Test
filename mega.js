04030e3699a8009ef53b2083ccef91cadfa2f0ba
JawadXDev <JawadXDev@outlook.com>
2025-04-11 08:09:11 +0500

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 import * as mega from 'megajs';
 
 const auth = {
-    email: 'jawadytx3@gmail.com',
+    email: 'hhsjawad246@gmail.com',
     password: 'JawadBisma355+',
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 };
